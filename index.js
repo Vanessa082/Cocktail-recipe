cfd70f59d249a5165c00c29f389b4f0c03e022a2
vanessa082 <wahvanessa22@gmail.com>
2024-03-07 10:08:22 +0100

    updated my fetchdrinkdata function adding event listenners to rhe prev and next buttons, deleted everything in my style,css start styling over again used the findindex metthod

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const drinkImage = document.querySelector(".drink-image");
-    const drinkName = document.querySelector(".drink-name");
-    const recipeButton = document.querySelector(".recipe-button");
-    const recipeDropdown = document.querySelector(".recipe-dropdown");
+document.addEventListener("DOMContentLoaded", function () {
+  const drinkImage = document.querySelector(".drink-image")
+  const drinkName = document.querySelector(".drink-name")
+  const recipeButton = document.querySelector(".recipe-button");
+  const prevButton = document.querySelector(".prev")
+  const nextButton = document.querySelector(".next")
+  const recipeDropdown = document.querySelector(".recipe-dropdown")
 
-    const fetchDrinkData = async () => {
-      const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=mojito");
-      const data = await response.json();
+  let drinkData = []
 
-      if (data.drinks) {
-        const drink = data.drinks[0];
-        drinkImage.src = drink.strDrinkThumb;
-        drinkName.textContent = drink.strDrink;
-        recipeDropdown.textContent = drink.strInstructions;
-      }
-    };
+  const fetchDrinkData = async () => {
+    const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php");
+    const data = await response.json()
 
-    fetchDrinkData();
+    if (data.drinks) {
+      drinkData = data.drinks
+      updateDrink(0);
+    }
+  }
 
-    recipeButton.addEventListener("click", function() {
-      recipeDropdown.style.display = "block";
-    });
+  const updateDrink = (i) => {
+    const drink = drinkData[i]
+    drinkImage.src = drink.strDrinkThumb;
+    drinkName.textContent = drink.strDrink;
+    recipeDropdown.textContent = drink.strInstructions;
+  }
 
+  fetchDrinkData();
+
+  recipeButton.addEventListener("click", function () {
+    recipeDropdown.style.display = "block";
   });
+
+  prevButton.addEventListener('click', () => {
+    const currentDrink = drinkData.findIndex((drink) => drink.strDrink === drinkName.textContent);
+    const prevDrink = (currentDrink - 1 + drinkData.length) % drinkData.length
+    updateDrink(prevDrink)
+    console.log(fetchDrinkData())
+  })
+
+  nextButton.addEventListener('click', () => {
+    const currentDrink = drinkData.findIndex((drink) => drink.strDrink === drinkName.textContent);
+    const nextDrink = (currentDrink + 1) % drinkData.length
+    updateDrink(nextDrink)
+    console.log(fetchDrinkData())
+  })
+});
