b07137f8c5aca9015e483fadc0d88c4d4d5f8604
vanessa082 <wahvanessa22@gmail.com>
2024-03-07 17:21:28 +0100

    styled the instruction section

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const selectedDrnk = document.querySelector('.selected-drink')
+  const selectedDrnk = document.querySelector('.carousel-container')
   const drinkImage = document.querySelector(".drink-image")
   const drinkName = document.querySelector(".drink-name")
   const recipeButton = document.querySelector(".recipe-button");
@@ -10,45 +10,49 @@ document.addEventListener("DOMContentLoaded", function () {
   let drinkData = []
   let idx = 0
 
-  let interval = setInterval(run, 2000)
-
   const fetchDrinkData = async () => {
     const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php");
     const data = await response.json()
-
+    
     if (data.drinks) {
       drinkData = data.drinks
       updateDrink(0)
     }
   }
-
+  
   const updateDrink = (idx) => {
     const drink = drinkData[idx]
     drinkImage.src = drink.strDrinkThumb;
     drinkName.textContent = drink.strDrink;
     recipeDropdown.textContent = drink.strInstructions;
   }
-
+  
   fetchDrinkData()
-
+  
   recipeButton.addEventListener("click", function () {
     recipeDropdown.style.display = "block";
   });
+  
+  // automatic transition
 
-const run = () =>{
-  idx++
-  changeDrinkData()
-}
+  const run = () =>{
+    idx++
+    changeDrinkData()
+  }
+
+  setInterval(run, 2000)
 
 const changeDrinkData = () =>{
   if(idx > drinkData.length -1){
     idx = 0
   } else if (idx < 0){
-    idx = drinkData,length
+    idx = drinkData.length
   }
 
   selectedDrnk.style.transform = `translateX(${-idx * 500}px)`
 }
+
+// buttons
 
   prevButton.addEventListener('click', () => {
     const currentDrink = drinkData.findIndex((drink) => drink.strDrink === drinkName.textContent);
